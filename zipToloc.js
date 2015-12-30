8799834c726bf9ea5b32cb6faa78e5a76af928c8
zhengshizhao <zhao.zhengshi@gmail.com>
2015-12-30 12:36:01 -0500

    modify zipToloc.js

--- zipToloc.js
+++ zipToloc.js
@@ -1,43 +1,60 @@
+//Import module https 
 var https = require("https");
 
-//console.log(url);
-
-function printmessage(city, state, summary){
-	console.log(city +", "+ state +" forecast today"+ ":\n" + summary);
+//Print forecast information
+function printmessage(city, state, temmin, temmax, humidity, windSpeed, summary){
+    console.log(city +", "+ state + ":\n" + "Temperature: " + temmin+ "°F to " + temmax + 
+        "°F Humidity: "+ humidity + " Windspeed: "+ windSpeed +"\nSummary: "+summary);
 
 }
 
+//Print error message
+function printError(error){
+   console.error(error.message);
+}
+
+
 function getforecast(zipcode){
 
+//Send a request to https://www.zipcodeapi.com/rest/<api_key>/info.<format>/<zip_code>/<units>
+// to get city, state, latitude, longitude etc from zip code. 
+
 var url = "https://www.zipcodeapi.com/rest/H3pllowmKQ0PRZkRzRCY1ZaeRTFzRrYzvoUQMrdxhdn8cHZMzCKBCUPtOu3GW1fr/info.json/" + zipcode +"/degrees";
-var request = https.get(url, function(response){
+
+     var request = https.get(url, function(response){
      var body = "";
+     //get data
      response.on('data', function(chunk){
              body += chunk;
      });
-     response.on('end', function(){
-     	var locInfo = JSON.parse(body);	
-     	//printmessage(locInfo.city, locInfo.state);
 
-     	var forecasturl = "https://api.forecast.io/forecast/c120a875c7be503b55d72763958bde75/"+ locInfo.lat+ "," + locInfo.lng;
-     	var req = https.get(forecasturl, function(res){
-     		 var weather = "";
-     		 res.on('data', function(chunk1){
+     response.on('end', function(){
+     	 var locInfo = JSON.parse(body);	
+        //Send a request to api.forecast.io to get weather information from latitude, longitude. 
+         var forecasturl = "https://api.forecast.io/forecast/c120a875c7be503b55d72763958bde75/"+ locInfo.lat+ "," + locInfo.lng;
+         var req = https.get(forecasturl, function(res){
+             var weather = "";
+             //get data
+             res.on('data', function(chunk1){
              weather += chunk1;
              });
+             //Show wether information
              res.on('end', function(){
-     			var locweather = JSON.parse(weather);	
-     	        //console.log(locweather.daily.data[0],locweather.daily.summary);  
-     	        printmessage(locInfo.city, locInfo.state, locweather.daily.summary);
-          });
-     
-     	});
+                var locweather = JSON.parse(weather);   
+                var WeatherToday = locweather.daily.data[0];  
+                printmessage(locInfo.city, locInfo.state, WeatherToday.temperatureMin,
+                WeatherToday.temperatureMax,WeatherToday.humidity, WeatherToday.windSpeed, WeatherToday.summary);
+            });
+             
+        req.on('error', printError);
+        });
+	
 
      });   
 });
 
+request.on('error', printError);
 }
 
-module.exports.getforecast = getforecast;
-
+ module.exports.getforecast = getforecast;
 
