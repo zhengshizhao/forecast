fd6088cfc50da08ae5bb10fe78c26c56b112ce5d
zhengshizhao <zhao.zhengshi@gmail.com>
2015-12-30 00:38:52 -0500

    modify forecast.js

--- forecast.js
+++ forecast.js
@@ -1,1 +1,7 @@
 
+//var http = require('http');
+var ziptoloc = require("./ziptoloc");
+//var zipcode  = "10024";
+var zipcode = process.argv.slice(2);
+zipcode.forEach(ziptoloc.getforecast);
+//console.log(process);
